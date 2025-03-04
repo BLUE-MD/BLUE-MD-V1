a658bc148b39baa347c67f12a24414a229b188fb
BLUE-MD <blueweb123888@gmail.com>
2025-03-05 00:44:34 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "",
+SESSION_ID: process.env.SESSION_ID || "SFBCibBa#u5cyogDIKL53lT9ueOZ4MTf7lMUeln5Kneqb17Nu_Ec",
 // add your Session Id 
 AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN || "true",
 // make true or false status auto seen
